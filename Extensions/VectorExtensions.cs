ad66e43240e610cb1894766f6d7589b71496645a
Arys <arys.steam@gmail.com>
2025-02-05 03:18:57 +0000

    Added documentation for Vector3 extensions

--- Extensions/VectorExtensions.cs
+++ Extensions/VectorExtensions.cs
@@ -3,43 +3,82 @@ using UnityEngine;
 
 namespace UnityToolkit.Extensions;
 
+/// <summary>
+/// Extensions for Unity's Vector3
+/// </summary>
 public static class VectorExtensions
 {
+	/// <summary>
+	/// Sets the x-axis for the specified Vector3.
+	/// </summary>
+	/// <param name="vector">The Vector3 to modify.</param>
+	/// <param name="value">The value to set to.</param>
+	/// <returns>The modified Vector3.</returns>
 	[UsedImplicitly]
 	public static Vector3 SetX(this Vector3 vector, float value)
 	{
 		vector.Set(value, vector.y, vector.z);
 		return vector;
 	}
-
+	
+	/// <summary>
+	/// Sets the y-axis for the specified Vector3.
+	/// </summary>
+	/// <param name="vector">The Vector3 to modify.</param>
+	/// <param name="value">The value to set to.</param>
+	/// <returns>The modified Vector3.</returns>
 	[UsedImplicitly]
 	public static Vector3 SetY(this Vector3 vector, float value)
 	{
 		vector.Set(vector.x, value, vector.z);
 		return vector;
 	}
-
+	
+	/// <summary>
+	/// Sets the z-axis for the specified Vector3.
+	/// </summary>
+	/// <param name="vector">The Vector3 to modify.</param>
+	/// <param name="value">The value to set to.</param>
+	/// <returns>The modified Vector3.</returns>
 	[UsedImplicitly]
 	public static Vector3 SetZ(this Vector3 vector, float value)
 	{
 		vector.Set(vector.x, vector.y, vector.z);
 		return vector;
 	}
-
+	
+	/// <summary>
+	/// Adds to the x-axis for the specified Vector3.
+	/// </summary>
+	/// <param name="vector">The Vector3 to modify.</param>
+	/// <param name="value">The value to add.</param>
+	/// <returns>The modified Vector3.</returns>
 	[UsedImplicitly]
 	public static Vector3 AddX(this Vector3 vector, float value)
 	{
 		vector.Set(vector.x + value, vector.y, vector.z);
 		return vector;
 	}
-
+	
+	/// <summary>
+	/// Adds to the y-axis for the specified Vector3.
+	/// </summary>
+	/// <param name="vector">The Vector3 to modify.</param>
+	/// <param name="value">The value to add.</param>
+	/// <returns>The modified Vector3.</returns>
 	[UsedImplicitly]
 	public static Vector3 AddY(this Vector3 vector, float value)
 	{
 		vector.Set(vector.x, vector.y + value, vector.z);
 		return vector;
 	}
-
+	
+	/// <summary>
+	/// Adds to the z-axis for the specified Vector3.
+	/// </summary>
+	/// <param name="vector">The Vector3 to modify.</param>
+	/// <param name="value">The value to add.</param>
+	/// <returns>The modified Vector3.</returns>
 	[UsedImplicitly]
 	public static Vector3 AddZ(this Vector3 vector, float value)
 	{
